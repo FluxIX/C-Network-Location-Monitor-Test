0b1b62a928313e30eba3af24a55f71e325356ff6
Chris Herrick <cherrick@bb7.com>
2015-07-20 23:52:39 -0500

    Initial commit of the network monitor test.

---
+++ NetworkMonitorTest/NetworkMonitor.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Net;
+using System.Timers;
+
+namespace NetworkMonitoring
+{
+   public class NetworkMonitor : IDisposable
+   {
+      #region Events
+      #region Delegates
+      public delegate void MonitoringStartedDelegate( NetworkMonitor sender, EventArgs args );
+      public delegate void UpdatedDelegate( NetworkMonitor sender, NetworkMonitorEventArgs args );
+      public delegate void MonitoringStoppedDelegate( NetworkMonitor sender, EventArgs args );
+      #endregion
+
+      public event MonitoringStartedDelegate MonitoringStarted;
+      public event UpdatedDelegate Updated;
+      public event MonitoringStoppedDelegate MonitoringStopped;
+      #endregion
+
+      public NetworkMonitor( String address )
+         : this( new Uri( address ) )
+      {
+      }
+
+      public NetworkMonitor( Uri address )
+      {
+         MonitoredUri = address;
+      }
+
+      protected Uri monitoredUri;
+
+      public Uri MonitoredUri
+      {
+         get
+         {
+            return monitoredUri;
+         }
+
+         protected set
+         {
+            if( value != null )
+               monitoredUri = value;
+            else
+               throw new ArgumentNullException( "value", "Monitored URI cannot be null." );
+         }
+      }
+
+      protected Object MonitoringLock = new Object();
+
+      protected Boolean isMonitoring;
+
+      public Boolean IsMonitoring
+      {
+         get
+         {
+            lock( MonitoringLock )
+            {
+               return isMonitoring;
+            }
+         }
+
+         protected set
+         {
+            lock( MonitoringLock )
+            {
+               isMonitoring = value;
+            }
+         }
+      }
+
+      protected HttpWebRequest Request
+      {
+         get;
+         set;
+      }
+
+      /// <summary>
+      /// Begins monitoring with an update period of 5 seconds.
+      /// </summary>
+      /// <returns><c>true</c> if monitoring is started, <c>false</c> otherwise.</returns>
+      public virtual Boolean BeginMonitoring()
+      {
+         return BeginMonitoring( 5000 );
+      }
+
+      public virtual Boolean BeginMonitoring( Double monitorUpdateFrequency_ms )
+      {
+         Boolean result = false;
+
+         lock( MonitoringLock )
+         {
+            if( !IsMonitoring )
+            {
+               Request = WebRequest.CreateHttp( MonitoredUri );
+               HttpWebResponse response = Request.GetResponse() as HttpWebResponse;
+
+               result = IsMonitoring = response != null;
+            }
+         }
+
+         MonitorTimer = MakeTimer( monitorUpdateFrequency_ms );
+         MonitorTimer.Start();
+
+         if( MonitoringStarted != null )
+            MonitoringStarted.Invoke( this, new EventArgs() );
+
+         return result;
+      }
+
+      public virtual Boolean StopMonitoring()
+      {
+         Boolean result = false;
+
+         lock( MonitoringLock )
+         {
+            if( IsMonitoring )
+            {
+               MonitorTimer.Elapsed -= MonitorTimer_Elapsed;
+               MonitorTimer.Stop();
+
+               result = true;
+            }
+         }
+
+         if( result && MonitoringStopped != null )
+            MonitoringStopped.Invoke( this, new EventArgs() );
+
+         return result;
+      }
+
+      protected Timer MonitorTimer
+      {
+         get;
+         set;
+      }
+
+      protected Timer MakeTimer( Double monitorFrequency_ms )
+      {
+         Timer result = new Timer( monitorFrequency_ms );
+
+         result.Enabled = true;
+         result.Elapsed += MonitorTimer_Elapsed;
+
+         return result;
+      }
+
+      protected virtual void MonitorTimer_Elapsed( Object sender, ElapsedEventArgs e )
+      {
+         HttpWebResponse response = Request.GetResponse() as HttpWebResponse;
+
+         if( response != null && Updated != null )
+            Updated.Invoke( this, new NetworkMonitorEventArgs( response ) );
+      }
+
+      #region IDisposable Members
+      public void Dispose()
+      {
+         lock( MonitoringLock )
+         {
+            if( IsMonitoring && StopMonitoring() )
+               MonitorTimer.Dispose();
+         }
+      }
+      #endregion
+   }
+
+   public class NetworkMonitorEventArgs : EventArgs
+   {
+      public NetworkMonitorEventArgs( HttpWebResponse response )
+      {
+         Response = response;
+      }
+
+      public HttpWebResponse Response
+      {
+         get;
+         protected set;
+      }
+   }
+}
---
+++ NetworkMonitorTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkMonitoring;
+
+namespace NetworkMonitorTest
+{
+   class Program
+   {
+      private static NetworkMonitor Monitor;
+
+      static void Main( String[] args )
+      {
+         const String address = "http://www.google.com";
+
+         {
+            NetworkMonitor monitor = new NetworkMonitor( address );
+
+            monitor.MonitoringStarted += monitor_MonitoringStarted;
+            monitor.MonitoringStopped += monitor_MonitoringStopped;
+            monitor.Updated += monitor_Updated;
+
+            Monitor = monitor;
+         }
+
+         Monitor.BeginMonitoring( 1500 );
+
+         Console.Out.WriteLine( "Press any key to stop monitoring..." );
+         Console.In.Read();
+
+         Monitor.StopMonitoring();
+      }
+
+      static void monitor_MonitoringStarted( NetworkMonitor sender, EventArgs args )
+      {
+         DateTime now = DateTime.Now;
+
+         Console.Out.WriteLine( String.Format( "{0}: Monitoring {1} started.", now.ToString(), sender.MonitoredUri.AbsoluteUri ) );
+      }
+
+      static void monitor_MonitoringStopped( NetworkMonitor sender, EventArgs args )
+      {
+         DateTime now = DateTime.Now;
+
+         Console.Out.WriteLine( String.Format( "{0}: Monitoring {1} stopped.", now.ToString(), sender.MonitoredUri.AbsoluteUri ) );
+      }
+
+      static void monitor_Updated( NetworkMonitor sender, NetworkMonitorEventArgs args )
+      {
+         DateTime now = DateTime.Now;
+
+         Console.Out.WriteLine( String.Format( "{0}: Monitoring {1} updated. Response status: {2} ({3})", now.ToString(), sender.MonitoredUri.AbsoluteUri, args.Response.StatusCode, Convert.ToUInt16( args.Response.StatusCode ) ) );
+      }
+   }
+}
